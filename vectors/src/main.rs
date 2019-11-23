6dddb683b5926f37ac6373959b341e34da732ee4
weiguo.ni <neefrankie@gmail.com>
2019-11-23 11:47:52 +0800

    vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+
+    let v: Vec<i32> = Vec::new();
+
+    let v2 = vec![1, 2, 3];
+
+    let mut v3 = Vec::new();
+
+    v3.push(5);
+    v3.push(6);
+    v3.push(7);
+    v3.push(8);
+}
