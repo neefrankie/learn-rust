e1f42c68d96bbd7b41ef6f168299ee294110922f
neefrankie <neefrankie@gmail.com>
2020-01-04 20:44:35 +0800

    greprs

---
+++ error_handing/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs::File;
+use std::io::{ErrorKind, Read};
+use std::io;
+
+fn main() {
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.text") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!("Tried to create file but there was a problem: {:?}", e)
+                },
+            }
+        },
+        Err(error) => {
+            panic!("There was a problem opening the file {:?}", error)
+        },
+    };
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
